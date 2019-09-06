f8b135daf23248cea411806a51548dc33c9fd620
BwayCer <bwaycer@gmail.com>
2019-09-06 15:35:59 +0800

    增補 雙層頁面

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -18,9 +18,79 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(
-        body: new Center(
-          child: new Text('Hello World'),
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Container(
+          margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: new Text(
+            '首頁',
+            style: new TextStyle(
+              fontSize: 48.0,
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          height: 60.0,
+          color: Colors.yellow,
+          child: new RaisedButton(
+            child: new Text(
+              '進入',
+              style: new TextStyle(
+                fontSize: 20.0,
+                color: Colors.grey[100],
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            color: Colors.blue,
+            onPressed: () => Navigator.push(
+              context,
+              new MaterialPageRoute(
+                builder: (context) => new ApDrawer(),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ApDrawer extends StatelessWidget {
+  List<dynamic> _apXxx;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('鍵值表'),
+      ),
+      // TODO 不能加 new
+      // body: (_apXxx?.isEmpty ?? true) ? emptyNotFound() : showData(),
+      body: emptyNotFound(),
+    );
+  }
+
+  Widget emptyNotFound() {
+    return new Center(
+      child: new Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: new Text(
+          '未有任何資料',
+          style: new TextStyle(
+            fontSize: 20.0,
+          ),
         ),
       ),
     );
