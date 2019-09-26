a7a3b759dbbcfd0c476478eba8593cc9a595f28a
BwayCer <bwaycer@gmail.com>
2019-09-26 18:15:32 +0800

    增補 基於熱重載及時編譯 JIT 的改寫

--- pkg/first_app/lib/main.dart
+++ pkg/first_app/lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
-    ));
+// 圖形介面的 Hello world!
+// void main() => runApp(new Text(
+//       'Hello World!',
+//       textDirection: TextDirection.ltr,
+//     ));
+
+// 基於熱重載及時編譯 JIT 的改寫
+// void main() => runApp(myAppBuild());
+void main() {
+  print('main() - 初始化時被執行');
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myAppBuild();
+  }
+}
+
+Widget myAppBuild() {
+  print('MyApp#build() - 初始化及熱重載時被執行');
+
+  return new Text(
+    'Hello World!',
+    textDirection: TextDirection.ltr,
+  );
+}
