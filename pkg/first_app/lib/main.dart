31be96642cc23f1defeb59f555b2cf2ef0da0061
BwayCer <bwaycer@gmail.com>
2019-09-26 18:29:01 +0800

    增補 MaterialApp 框

--- pkg/first_app/lib/main.dart
+++ pkg/first_app/lib/main.dart
@@ -23,8 +23,18 @@ class MyApp extends StatelessWidget {
 Widget myAppBuild() {
   print('MyApp#build() - 初始化及熱重載時被執行');
 
-  return new Text(
-    'Hello World!',
-    textDirection: TextDirection.ltr,
+  return new MaterialApp(
+    // `title` 沒什麼用的意思
+    //   * https://api.flutter.dev/flutter/material/MaterialApp/title.html
+    //   * https://stackoverflow.com/questions/50615006/where-does-the-title-of-material-app-used-in-flutter
+    title: '第一個 Flutter 應用程式',
+    theme: new ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: new Scaffold(
+      body: new Center(
+        child: new Text('Hello World!'),
+      ),
+    ),
   );
 }
